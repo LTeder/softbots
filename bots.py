57c84d663b1e580da8cd55d786ea4e577bf64b7e
LTeder <l.teder@columbia.edu>
2022-11-20 20:30:27 -0500

    Optimized simulation speed and accuracy
    Simulation Mass attributes position, velocity, accel., and force are now PyTorch tensors
    This allows for vector computation with CUDA parallelization
    Additionally, it simplifies certain routines significantly
    A couple unused attributes were removed from Universe.simulate()

--- bots.py
+++ bots.py
@@ -2,18 +2,26 @@ import torch
 import numpy as np
 import matplotlib.pyplot as plt
 
+def dist(p1, p2):
+    # p1 and p2 are torch tensors
+    return (p2 - p1).pow(2).sum().pow(0.5).item()
+
+def normalize(p):
+    # p is a torch tensor
+    magnitude = p.pow(2).sum().pow(0.5).item()
+    return p / magnitude if magnitude else 0
+    
 class Mass:
-    def __init__(self, m=1, p=[0,0,0], v=[0, 0, 0], a=[0, 0, 0], F=[0, 0, 0], grounded=False, damping=0):
+    def __init__(self, p = [0, 0, 0], v = [0, 0, 0], a = [0, 0, 0], F = [0, 0, 0], m = 1):
         self.m = m
-        self.p = p
-        self.v = v
-        self.a = a
-        self.F = F
-        self.grounded = grounded
-        self.damping = damping
+        self.p = torch.tensor(p, dtype = torch.float64, device = device) # Position
+        self.v = torch.tensor(v, dtype = torch.float64, device = device) # Velocity
+        self.a = torch.tensor(a, dtype = torch.float64, device = device) # Acceleration
+        self.F = torch.tensor(F, dtype = torch.float64, device = device) # Force
 
 class Spring:
-    def __init__(self, Masses, L_0=1, k=1, m1_idx=0, m2_idx=0, status='steady', damping = 0.0, omega = 1, c = 0):
+    def __init__(self, Masses, L_0 = 1, k = 1, m1_idx = 0, m2_idx = 0, status = 'steady',
+                 damping = 0.0, b = 0, omega = 0, c = 0):
         """
         m1: index in associated Mass list
         m2: index in associated Mass list
@@ -29,24 +37,17 @@ class Spring:
         self.m2 = Masses[m2_idx]
         self.status = status
         self.damping = damping
+        self.b = b
         self.omega = omega
-        self.c = c
-        
-        self.L = self.dist(self.m1.p, self.m2.p)
+        self.c = c        
         self.set_L_1()
-
-    def dist(self, p1, p2):
-        """
-        p1: (x1, y1, z1)
-        p2: (x2, y2, z2)
-        """
-        return ( (p1[0]-p2[0])**2 + (p1[1]-p2[1])**2 + (p1[2]-p2[2])**2)**0.5
         
     def set_L_1(self, t = 0):
-        self.L_1 = self.L_0 * (1 + self.c * np.sin(2*np.pi * self.omega * t))
+        self.refresh_L()
+        self.L_1 = self.L_0 * (1 + self.b * np.sin(2 * np.pi * self.omega * t + self.c))
           
     def refresh_L(self):
-        self.L = self.dist(self.m1.p, self.m2.p)
+        self.L = dist(self.m1.p, self.m2.p)
         
     def force(self):
         """
@@ -55,237 +56,165 @@ class Spring:
         
         returns force magnitude (direction to be determined outside)
         """
-        return self.k * (self.L - self.L_1) * (1-self.damping)
+        return self.k * (self.L - self.L_1) * (1 - self.damping)
     
     def energy(self):
-        return 1/2 * self.k * (self.L - self.L_1)**2 
+        return 1/2 * self.k * (self.L - self.L_1)**2
 
 class Universe:
-    def __init__(self, Masses, Springs, dt, box_dims = [10, 10, 10],
-                 K_G=1e5, g=-9.812, grav=False, damping = 0, mu=1.0):
+    def __init__(self, Masses, Springs, dt, box_dims = [20, 20, 20], K_G = 1e5,
+                 grav = -9.812, damping = 0, mu = 1.0, verbose = False):
         self.Masses = Masses
         self.Springs = Springs
         self.dt = dt
+
         self.box_dims = box_dims # bounds
-        self.K_G=K_G # resistance for walls
-        self.g = g # gravitational constant
-        self.grav = grav
+        self.K_G = K_G # resistance for walls
+        self.grav = grav # gravitational constant
         self.damping = damping
         self.mu = mu # coefficient of friction
-        
-        self.DIMENSIONS = 3
-        
+        self.verbose = verbose
+
         for s in self.Springs:
             s.damping = self.damping
-            
-        self.ax = plt.axes(projection='3d')
-
-        self.points = []
-        
         self.kinetic = 0
-        self.potential_springs = 0
+        self.potential_springs = 0 # includes bounds
         self.potential_gravity = 0
-        self.energies = [] # kinetic, spring potential (including bounds), gravitational potential
-            
-    def calculate_mass_center(self):
-        """
-        Loop through all masses and return the center of mass
-        """
-        pass
-
-    def normalize_2d(self, p):
-        x, y = p
-        
-        if x==y==0:
-            return p
-        
-        mag = (x**2 + y**2)**0.5
-        return [x/mag, y/mag]
-
-    def normalize(self, p):
-        x, y, z = p
-        
-        if x==y==z==0:
-            return p
-        
-        mag = (x**2 + y**2 + z**2)**0.5
-        return [x/mag, y/mag, z/mag]
-
-    def integration_step(self, t=0, verbose=False):
-        # velocity and position carry over, Force and acceleraton are recalculated at each time step
-        
-        # reset forces and accelerations
-        for m in self.Masses:
-            m.F = [0, 0, 0]
-            m.a = [0, 0, 0]
-            
-        # reset energies
-        self.potential_springs = 0
-        self.potential_gravity = 0
-        self.kinetic = 0
-        
-        ### calculate spring forces
-        for s in self.Springs:
-            magnitude = s.force()
-                        
-            if magnitude == 0:
-                s.status = 'steady'
-            elif magnitude > 0:
-                s.status = 'stretched'
-            else:
-                s.status = 'compressed'
-            
-            # force on m1
-            m1_direction = [s.m1.p[0] - s.m2.p[0], s.m1.p[1] - s.m2.p[1]]
-            m1_direction = [s.m2.p[0] - s.m1.p[0], s.m2.p[1] - s.m1.p[1], s.m2.p[2] - s.m1.p[2]]
-            m1_direction = self.normalize(m1_direction)
-            m1_force = [m1_direction[axis] * magnitude for axis in range(self.DIMENSIONS)]
-            
-            s.m1.F[0] += m1_force[0] # x
-            s.m1.F[1] += m1_force[1] # y
-            s.m1.F[2] += m1_force[2] # z
-            
-            # force on m2
-#             m2_direction = [s.m2.p[0] - s.m1.p[0], s.m2.p[1] - s.m1.p[1]]
-            m2_direction = [s.m1.p[0] - s.m2.p[0], s.m1.p[1] - s.m2.p[1], s.m1.p[2] - s.m2.p[2]]
-
-            m2_direction = self.normalize(m2_direction)
-            m2_force = [m2_direction[axis] * magnitude for axis in range(self.DIMENSIONS)]
-            
-            s.m2.F[0] += m2_force[0] # x
-            s.m2.F[1] += m2_force[1] # y
-            s.m2.F[2] += m2_force[2] # z
-            
-            ### add spring potential energy
-            self.potential_springs += s.energy()
-        
-        ### update Mass Forces
-        for m in self.Masses:
-            
-            ### gravity
-            if self.grav:
-                m.F[2] += self.g * m.m
-                
-                # calculate gravitational potential energy
-                self.potential_gravity += - self.g * m.m * m.p[2] # based on position 
-                
-                # calculate kinetic energy
-                self.kinetic += 1/2 * m.m * (m.v[0]**2 + m.v[1]**2 + m.v[2]**2)
-                
-            # ground
-            if m.p[2] < 0:
-                # normal force
-                normal_force = self.K_G * (0 - m.p[2])
-                m.F[2] += normal_force
-                # calculate elastic energy
-                self.potential_springs += 1/2 * self.K_G * (0 - m.p[2])**2
-
-                ## calculate friction force
-                if not (m.p[0] == 0 and m.p[1] == 0):
-                    # calculate direction of movement
-                    x_normed, y_normed = self.normalize_2d(m.v[:2]) 
-                    # oppose x direction
-                    m.F[0] -= x_normed * self.mu * normal_force
-
-                    # oppose y direction
-                    m.F[1] -= y_normed * self.mu * normal_force
-            
-        ### calculate energies (note: should this be before or after the points are adjusted?)
-        ### update a, v, p
-        for m in self.Masses:
-            # update acceleration
-            m.a[0] = m.F[0] / m.m # x
-            m.a[1] = m.F[1] / m.m # y
-            m.a[2] = m.F[2] / m.m # z
-            
-            # update velocity
-            m.v[0] += m.a[0] * self.dt
-            m.v[1] += m.a[1] * self.dt
-            m.v[2] += m.a[2] * self.dt
-            
-            # update position
-            m.p[0] += m.v[0] * self.dt
-            m.p[1] += m.v[1] * self.dt
-            m.p[2] += m.v[2] * self.dt
-        
-        ### update spring lengths
-        for s in self.Springs:
-            s.refresh_L()
-            s.set_L_1(t)
-           
-        if verbose:
-            for m in self.Masses:
-                print(f"m.F = {m.F}, m.a = {m.a}, m.v = {m.v}, m.p = {m.p}")
-                
-        # return eneergies [kinetic, spring potential (including bounds), grav potential]
-        return [self.kinetic, self.potential_springs, self.potential_gravity]
-
-    def get_points(self):
-        points = []
-        for m in self.Masses:
-            points.append(m.listify())
-        return points
+        self.points = None
+        self.energies = None
     
-    def display_frame(self, save=False, filename=None):
+    def display_frame(self, save = False, filename = None):
         fig = plt.figure()
-        ax = plt.axes(projection='3d')
+        ax = plt.axes(projection = '3d')
         ax.cla()
-
-        ax.axes.set_xlim3d(left=0, right=self.box_dims[0]) 
-        ax.axes.set_ylim3d(bottom=0, top=self.box_dims[1]) 
-        ax.axes.set_zlim3d(bottom=0, top=self.box_dims[2]) 
+        ax.axes.set_xlim3d(left = 0, right = self.box_dims[0]) 
+        ax.axes.set_ylim3d(bottom = 0, top = self.box_dims[1]) 
+        ax.axes.set_zlim3d(bottom = 0, top = self.box_dims[2]) 
         
         # scatter plot of Point Masses
         for m in self.Masses:
-            ax.scatter3D(m.p[0], m.p[1], m.p[2], color='k')
+            ax.scatter3D(*m.p.cpu(), color = 'k')
         
         # plot of Springs
         for s in self.Springs:
-            x = [s.m1.p[0], s.m2.p[0]]
-            y = [s.m1.p[1], s.m2.p[1]]
-            z = [s.m1.p[2], s.m2.p[2]]
+            p1 = s.m1.p.cpu()
+            p2 = s.m2.p.cpu()
+            x = [p1[0], p2[0]]
+            y = [p1[1], p2[1]]
+            z = [p1[2], p2[2]]
             if s.status == 'steady':
                 color = 'green'
             elif s.status == 'stretched':
                 color = 'red'
             else:
                 color = 'blue'
-            ax.plot3D(x, y, z, color=color)
+            ax.plot3D(x, y, z, color = color)
 
-        
-        # display or save
+        fig.set_size_inches(10, 10)
+
         if save:
             fig.savefig(filename)
-            
-        plt.close()
-    
-    def simulate(self, t, save = False, filename = '', verbose=False):
-        """
-        t: list of time points, spaced by dt
-        
-        filename: template for beginning of filename
-        
-        returns a list of 
-        """
-        
-        length = len(t)
-        
-        digit_length = len(str(length))
-        
-        for i, t_ in enumerate(t):
-            # do integration step
-            energies = self.integration_step(t=t_, verbose=verbose)
-            
-            self.energies.append(energies)
-            
-            # get filename
-            i_str = str(i)
-            num_digits = len(i_str)
-            
-            frame_filename = filename + '0'*(digit_length - num_digits) + i_str
-            
-            self.display_frame(save=save, filename=frame_filename)
 
-            self.points.append([m.p.copy() for m in self.Masses])
+        return ax
+
+    def center_of_mass_horizontal(self):
+        total_mass = 0
+        x_center = 0
+        y_center = 0
+        for m in self.Masses:
+            total_mass += m.m
+            x_center += m.p[0].item() * m.m
+            y_center += m.p[1].item() * m.m
+        x_center /= total_mass
+        y_center /= total_mass
+        return torch.tensor([x_center, y_center], device = device)
+
+    def integration_step(self):
+        # velocity and position carry over, force and acceleraton are recalculated at each time step
+        # reset forces and energies
+        for m in self.Masses:
+            m.F *= 0
+        self.potential_springs = 0
+        self.potential_gravity = 0
+        self.kinetic = 0
+
+        ### calculate spring forces
+        for s in self.Springs:
+            magnitude = s.force()
+            # update status
+            if magnitude == 0:
+                s.status = 'steady'
+            elif magnitude > 0:
+                s.status = 'stretched'
+            else:
+                s.status = 'compressed'
+            # update force tensor attributes
+            s.m1.F += normalize(s.m2.p - s.m1.p) * magnitude
+            s.m2.F += normalize(s.m1.p - s.m2.p) * magnitude
+            # add spring potential energy
+            self.potential_springs += s.energy()
+
+        ### update Mass Forces
+        for m in self.Masses:
+            if self.grav:
+                m.F[2] += self.grav * m.m
+                # calculate gravitational potential energy
+                self.potential_gravity -= self.grav * m.m * m.p[2].item() # based on position 
+                # calculate kinetic energy
+                self.kinetic += 1/2 * m.m * m.v.pow(2).sum().pow(0.5).item() ** 2
+            # ground interaction
+            if m.p[2] < 0:
+                normal_force = self.K_G * m.p[2].item()
+                # calculate elastic energy within ground
+                self.potential_springs += 1/2 * self.K_G * m.p[2].item() ** 2
+                ## calculate friction force
+                #m.F -= normalize(m.v) * self.mu * normal_force
+                m.F[:2] -= normalize(m.v[:2]) * self.mu * normal_force
+                m.F[2] -= normal_force
+
+        ### update a, v, p
+        for m in self.Masses:
+            m.a = m.F / m.m # acceleration
+            m.v += m.a * self.dt # velocity
+            m.p += m.v * self.dt # position
+
+        ### update spring lengths
+        for s in self.Springs:
+            s.set_L_1(self._t) # _t is updated before integration_step is called
+
+        if self.verbose:
+            for i, m in enumerate(self.Masses):
+                print(f"### Mass {i} ###\n"
+                      f"m.F = {m.F}, m.a = {m.a}, m.v = {m.v}, m.p = {m.p}")
+    
+    def simulate(self, t):
+        """
+        t: iterable of time points, spaced by dt
+        
+        returns the robot mass points, its energies, and its total distance traveled
+        """
+        digit_length = len(str(len(t)))
+        frames = []
+        start_pos_horizontal = self.center_of_mass_horizontal()
+        
+        self.energies = torch.zeros((len(t), 3), device = device)
+        self.points = torch.zeros((len(t), len(Masses), 3), device = device)
+
+        if self.verbose:
+            for i, m in enumerate(self.Masses):
+                print(f"### Mass {i} ###\n"
+                      f"m.F = {m.F}, m.a = {m.a}, m.v = {m.v}, m.p = {m.p}")
+        
+        for i, _t in tqdm(enumerate(t), total = len(t)):
+            self._t = _t # updated for Spring.set_L_1() called next
+            self.integration_step() # updates energies and self.Masses
+            self.energies[i] = torch.tensor([self.kinetic, self.potential_springs,
+                                             self.potential_gravity], device = device)
+            # update points iteratively with Masses
+            for j, m in enumerate(self.Masses):
+                self.points[i][j] = m.p.clone()
+
+        end_pos_horizontal = self.center_of_mass_horizontal()
+        total_dist_horizontal = dist(start_pos_horizontal, end_pos_horizontal)
             
-        return self.points, self.energies
+        return self.points, self.energies, anim, total_dist_horizontal
--- test_simulate_bot.py
+++ test_simulate_bot.py
@@ -1,67 +1,128 @@
 import torch
 import numpy as np
 import matplotlib.pyplot as plt
+from tqdm.auto import tqdm
 
 from bots import Spring, Mass, Universe
 
+# Setup execution device for torch tensors
+if torch.cuda.is_available():
+    device = torch.device("cuda:0")
+    torch.cuda.set_device(device)
+else:
+    device = torch.device("cpu")
+device = torch.device("cpu")
+device
 
-Masses = [Mass(m=0.1, p=[2, 2, 0], v=[0,0,0]),
-          Mass(m=0.1, p=[5, 2, 0], v=[0,0,0]),
-          Mass(m=0.1, p=[2, 5, 0], v=[0,0,0]),
-          Mass(m=0.1, p=[5, 5, 0], v=[0,0,0]),
-         
-          Mass(m=0.1, p=[2, 2, 3], v=[0,0,0]),
-          Mass(m=0.1, p=[5, 2, 3], v=[0,0,0]),
-          Mass(m=0.1, p=[2, 5, 3], v=[0,0,0]),
-          Mass(m=0.1, p=[5, 5, 3], v=[0,0,0])]
+class Soft_Spring(Spring):
+    def __init__(self, Masses, L_0=1, k=1000, m1_idx=0, m2_idx=0,
+                 status='steady', damping=0.0):
+        super().__init__(Masses, L_0=L_0, k=k, m1_idx=m1_idx, m2_idx=m2_idx,
+                         status=status, damping=damping, b=0, omega=0, c=0)
 
-Springs = [Spring(Masses, L_0=3, k=2000, m1_idx=0, m2_idx=1, omega=1, c=0.25),
-           Spring(Masses, L_0=3, k=1500, m1_idx=1, m2_idx=3, omega=1, c=0.25),
-           Spring(Masses, L_0=3, k=1000, m1_idx=0, m2_idx=2, omega=1, c=0.25),
-           Spring(Masses,L_0=3, k=1500, m1_idx=2, m2_idx=3, omega=1, c=0.25),
-           
-           Spring(Masses, L_0=3, k=2000, m1_idx=4, m2_idx=5, omega=1, c=0.25), 
-           Spring(Masses, L_0=3, k=1500, m1_idx=5, m2_idx=7, omega=1, c=0.25),
-           Spring(Masses, L_0=3, k=1000, m1_idx=4, m2_idx=6, omega=1, c=0.25),
-           Spring(Masses,L_0=3, k=1500, m1_idx=6, m2_idx=7, omega=1, c=0.25),
-           
-           Spring(Masses, L_0=3, k=2000, m1_idx=0, m2_idx=4, omega=1, c=0.25), 
-           Spring(Masses, L_0=3, k=1500, m1_idx=1, m2_idx=5, omega=1, c=0.25),
-           Spring(Masses, L_0=3, k=1000, m1_idx=2, m2_idx=6, omega=1, c=0.25),
-           Spring(Masses,L_0=3, k=1500, m1_idx=3, m2_idx=7, omega=1, c=0.25),
-          
-          # inside cross edges
-           Spring(Masses, L_0=27**0.5, k = 5000, m1_idx= 0, m2_idx = 7, omega=1, c=0.25),
-           Spring(Masses, L_0=27**0.5, k = 5000, m1_idx = 3, m2_idx = 4, omega=1, c=0.25),
-           Spring(Masses, L_0=27**0.5, k = 5000, m1_idx= 1, m2_idx = 6, omega=1, c=0.25),
-           Spring(Masses, L_0=27**0.5, k = 5000, m1_idx = 5, m2_idx = 2, omega=1, c=0.25)]
+class Hard_Spring(Spring):
+    def __init__(self, Masses, L_0=1, k=20000, m1_idx=0, m2_idx=0,
+                 status='steady', damping=0.0):
+        super().__init__(Masses, L_0=L_0, k=k, m1_idx=m1_idx, m2_idx=m2_idx,
+                         status=status, damping=damping, b=0, omega = 0, c=0)
 
-dt = 0.0005
-uni = Universe(Masses, Springs, dt, grav=True)
+class ExpandContract(Spring):
+    def __init__(self, Masses, L_0=1, k=5000, m1_idx=0, m2_idx=0,
+                 status='steady', damping=0.0, b=0.1, omega=0.5):
+        super().__init__(Masses, L_0=L_0, k=k, m1_idx=m1_idx, m2_idx=m2_idx,
+                         status=status, damping=damping, b=b, omega = omega, c=0)
 
-t = np.arange(0, 3.0, dt)
-points, energies = uni.simulate(t, save = False, filename='Crossed3DCube/frame', verbose=False)
+class ContractExpand(Spring):
+    def __init__(self, Masses, L_0=1, k=5000, m1_idx=0, m2_idx=0,
+                 status='steady', damping=0.0, b=0.1, omega=0.5):
+        super().__init__(Masses, L_0=L_0, k=k, m1_idx=m1_idx, m2_idx=m2_idx,
+                         status=status, damping=damping, b=b, omega = omega, c=np.pi)
 
-points_tensor = torch.tensor(points)
-torch.save(points_tensor, 'BreathingCube3D_dt0.0005.pt')
 
-# plot energies
-kinetic = [e[0] for e in energies]
-potential_springs = [e[1] for e in energies]
-potential_gravity = [e[2] for e in energies]
-sum = []
-for i in range(len(t)):
-    sum.append(kinetic[i] + potential_springs[i] + potential_gravity[i])
+Masses = [
+    ## upper layer
+        # left side (0, 2, 4, 6)            # right side (1, 3, 5, 7)
+         Mass(m=1, p=[9, 2, 3], v=[0,0,0]), Mass(m=0.1, p=[9, 5, 3], v=[0,0,0]),
+         Mass(m=1, p=[6, 2, 3], v=[0,0,0]), Mass(m=0.1, p=[6, 5, 3], v=[0,0,0]),
+         Mass(m=1, p=[3, 2, 3], v=[0,0,0]), Mass(m=0.1, p=[3, 5, 3], v=[0,0,0]),
+         Mass(m=1, p=[0, 2, 3], v=[0,0,0]), Mass(m=0.1, p=[0, 5, 3], v=[0,0,0]),
 
-plt.plot(t, kinetic, label = 'kinetic')
-plt.plot(t, potential_springs, label = 'potential (springs)')
-plt.plot(t, potential_gravity, label = 'potential (gravity)')
-plt.plot(t, sum, label = 'total energy')
-plt.legend()
+    ## lower layer
+        # left side (8, 10, 12, 14)          # right side (9, 11, 13, 15)
+         Mass(m=1, p=[9, 2, 0], v=[0,0,0]), Mass(m=0.1, p=[9, 5, 0], v=[0,0,0]),
+         Mass(m=1, p=[6, 2, 0], v=[0,0,0]), Mass(m=0.1, p=[6, 5, 0], v=[0,0,0]),
+         Mass(m=1, p=[3, 2, 0], v=[0,0,0]), Mass(m=0.1, p=[3, 5, 0], v=[0,0,0]),
+         Mass(m=1, p=[0, 2, 0], v=[0,0,0]), Mass(m=0.1, p=[0, 5, 0], v=[0,0,0])]
 
-plt.xlabel('Time (s)')
-plt.title('Energy graph (breathing cube, $\omega = 1, c = 0.25$)')
-plt.ylabel('Energy (Joules)')
+Springs = [
+    ### cross Y springs
+    Hard_Spring(Masses, L_0 = 3, m1_idx = 0, m2_idx = 1),
+    Hard_Spring(Masses, L_0 = 3, m1_idx = 2, m2_idx = 3),
+    Hard_Spring(Masses, L_0 = 3, m1_idx = 4, m2_idx = 5),
+    Hard_Spring(Masses, L_0 = 3, m1_idx = 6, m2_idx = 7),
+    Hard_Spring(Masses, L_0 = 3, m1_idx = 8, m2_idx = 9),
+    Hard_Spring(Masses, L_0 = 3, m1_idx = 10, m2_idx = 11),
+    Hard_Spring(Masses, L_0 = 3, m1_idx = 12, m2_idx = 13),
+    Hard_Spring(Masses, L_0 = 3, m1_idx = 14, m2_idx = 15),
 
-fig = plt.gcf()
-fig.set_size_inches(15, 10)
+    ### Z springs
+    # left
+    Hard_Spring(Masses, L_0 = 3, m1_idx = 0, m2_idx = 8),
+    Hard_Spring(Masses, L_0 = 3, m1_idx = 2, m2_idx = 10),
+    Hard_Spring(Masses, L_0 = 3, m1_idx = 4, m2_idx = 12),
+    Hard_Spring(Masses, L_0 = 3, m1_idx = 6, m2_idx = 14),
+    # right
+    Hard_Spring(Masses, L_0 = 3, m1_idx = 1, m2_idx = 9),
+    Hard_Spring(Masses, L_0 = 3, m1_idx = 3, m2_idx = 11),
+    Hard_Spring(Masses, L_0 = 3, m1_idx = 5, m2_idx = 13),
+    Hard_Spring(Masses, L_0 = 3, m1_idx = 7, m2_idx = 15),
+
+    ### 1st and 2nd cubes (expanding -> contracting)
+    ## 1st cube
+    # diagonal
+    ContractExpand(Masses, L_0 = 27**0.5, m1_idx = 0, m2_idx = 11),
+    ContractExpand(Masses, L_0 = 27**0.5, m1_idx = 1, m2_idx = 10),
+    ContractExpand(Masses, L_0 = 27**0.5, m1_idx = 8, m2_idx = 3),
+    ContractExpand(Masses, L_0 = 27**0.5, m1_idx = 9, m2_idx = 2),
+
+    # x horizontal
+    ContractExpand(Masses, L_0 = 3, m1_idx = 0, m2_idx = 2),
+    ContractExpand(Masses, L_0 = 3, m1_idx = 1, m2_idx = 3),
+    ContractExpand(Masses, L_0 = 3, m1_idx = 8, m2_idx = 10),
+    ContractExpand(Masses, L_0 = 3, m1_idx = 9, m2_idx = 11),    
+
+    ## 2nd cube
+    # diagonal
+    ContractExpand(Masses, L_0 = 27**0.5, m1_idx = 2, m2_idx = 13),
+    ContractExpand(Masses, L_0 = 27**0.5, m1_idx = 3, m2_idx = 12),
+    ContractExpand(Masses, L_0 = 27**0.5, m1_idx = 10, m2_idx = 5),
+    ContractExpand(Masses, L_0 = 27**0.5, m1_idx = 11, m2_idx = 4),
+
+    # x horizontal
+    ContractExpand(Masses, L_0 = 3, m1_idx = 2, m2_idx = 4),
+    ContractExpand(Masses, L_0 = 3, m1_idx = 3, m2_idx = 5),
+    ContractExpand(Masses, L_0 = 3, m1_idx = 10, m2_idx = 12),
+    ContractExpand(Masses, L_0 = 3, m1_idx = 11, m2_idx = 13),    
+
+    ### last cube (contracting -> expanding)
+    # diagonal
+    ContractExpand(Masses, L_0 = 27**0.5, m1_idx = 4, m2_idx = 15),
+    ContractExpand(Masses, L_0 = 27**0.5, m1_idx = 5, m2_idx = 14),
+    ContractExpand(Masses, L_0 = 27**0.5, m1_idx = 12, m2_idx = 7),
+    ContractExpand(Masses, L_0 = 27**0.5, m1_idx = 13, m2_idx = 6),
+
+    # x horizontal
+    ContractExpand(Masses, L_0 = 3, m1_idx = 4, m2_idx = 6),
+    ContractExpand(Masses, L_0 = 3, m1_idx = 5, m2_idx = 7),
+    ContractExpand(Masses, L_0 = 3, m1_idx = 12, m2_idx = 14),
+    ContractExpand(Masses, L_0 = 3, m1_idx = 13, m2_idx = 15)]
+
+dt = 0.0001
+uni = Universe(Masses, Springs, dt, verbose=False)
+
+t = np.arange(0, 2.0, dt)
+result = uni.simulate(t)
+
+points_tensor = result[0].clone().detach()
+
+torch.save(points_tensor, 'Robot4_dt0.0001_damping0.25.pt')
